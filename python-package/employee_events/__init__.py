fb64421a1d6bdc85236105d41e22b91345574cf0
Samira Kristina Saak <67195816+sksaak@users.noreply.github.com>
2025-03-14 13:50:06 +0100

    corrected noqa exception

--- python-package/employee_events/__init__.py
+++ python-package/employee_events/__init__.py
@@ -1,4 +1,4 @@
 from .employee import Employee  # noqa: F401
 from .team import Team  # noqa: F401
 from .query_base import QueryBase  # noqa: F401
-from .sql_execution import *  # noqa: F401
+from .sql_execution import *  # noqa: F403
